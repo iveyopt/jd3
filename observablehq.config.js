619f8a5064a595fb6579c1a7a586116a6050af59
iveyopt <93614550+iveyopt@users.noreply.github.com>
2024-11-27 17:02:08 -0500

    Update observablehq.config.js

--- observablehq.config.js
+++ observablehq.config.js
@@ -23,7 +23,7 @@ export default {
   root: "src",
 
   // Some additional configuration options and their defaults:
-  theme: "near-midnight", // try "light", "dark", "slate", etc.
+  //theme: "near-midnight", // try "light", "dark", "slate", etc.
   header: "", // what to show in the header (HTML)
   footer: "", // what to show in the footer (HTML)
   sidebar: false, // whether to show the sidebar
