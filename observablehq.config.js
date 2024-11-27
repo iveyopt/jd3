5876a46af43fce8dcd430a9f10e8a4717428a177
iveyopt <93614550+iveyopt@users.noreply.github.com>
2024-11-27 14:27:48 -0500

    Update observablehq.config.js

--- observablehq.config.js
+++ observablehq.config.js
@@ -1,7 +1,7 @@
 // See https://observablehq.com/framework/config for documentation.
 export default {
   // The project’s title; used in the sidebar and webpage titles.
-  title: "Hello Framework",
+  title: "Reddit Data for the Fortune 500",
 
   // The pages and sections in the sidebar. If you don’t specify this option,
   // all pages will be listed in alphabetical order. Listing pages explicitly
@@ -26,9 +26,9 @@ export default {
   // theme: "default", // try "light", "dark", "slate", etc.
   // header: "", // what to show in the header (HTML)
   // footer: "Built with Observable.", // what to show in the footer (HTML)
-  // sidebar: true, // whether to show the sidebar
-  // toc: true, // whether to show the table of contents
-  // pager: true, // whether to show previous & next links in the footer
+  sidebar: false, // whether to show the sidebar
+  toc: false, // whether to show the table of contents
+  pager: false, // whether to show previous & next links in the footer
   // output: "dist", // path to the output root for build
   // search: true, // activate search
   // linkify: true, // convert URLs in Markdown to links
